a487911e91a122435ba2ab5cd932bc00e988495b
petr <petr.samusev01@gmail.com>
2026-04-19 18:56:55 +0700

    "Add ProductCard component"

--- src/pages/TVpage/TVPage.jsx
+++ src/pages/TVpage/TVPage.jsx
@@ -1,16 +1,31 @@
 import Header from "../../components/Header/Header";
 import Filter from "../../components/Filter/Filter";
 import SpecialDeal from "../../components/SpecialDeal/SpecialDeal";
+import ProductCard from "../../components/ProductCart/ProductCard";
 
 const TVPage = (props) => {
   const {className} = props;
   return (
     <div className="container">
       <Header />
-      <aside>
-        <Filter />
-        <SpecialDeal time={"0:53:50"} />
-      </aside>
+      <main>
+        <aside>
+          <Filter />
+          <SpecialDeal time={"0:53:50"} />
+        </aside>
+        <section className="products">
+          <div className="products__head">
+            <p className="products__count">8 products</p>
+          </div>
+          <div className="products__list">
+            <ProductCard
+              brand="brand"
+              title="sdffffffff"
+              price="100"
+            />
+          </div>
+        </section>
+      </main>
     </div>
   )
 }
